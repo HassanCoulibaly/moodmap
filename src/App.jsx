71159644f1034c351a4a4e8cbc74dfa834096e41
Shashwata Roy <sroy389@gatech.edu>
2026-03-28 15:58:46 -0400

    Revert "Merge branch 'main' of https://github.com/HassanCoulibaly/hacklanta-the-Cognitive-Coders"
    This reverts commit 1d3d62d89524759a38ce78039b8c0bc4edef2220, reversing
    changes made to b072d4b3b68abec04c7997d0de17b628e3ca64ef.

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,6 @@ import RecoveryFeed from './components/RecoveryFeed'
 import CrisisPanel from './components/CrisisPanel'
 import MoodJournal from './components/MoodJournal'
 import RelativeTime from './components/RelativeTime'
-import ReelGenerator from './components/ReelGenerator'
 
 export default function App() {
   const [pins, setPins] = useState(SEED_PINS)
@@ -753,8 +752,6 @@ export default function App() {
           >
             {waving ? 'Spreading…' : loading ? 'Analysing…' : 'Simulate Stress Wave'}
           </button>
-
-          <ReelGenerator pins={pins} />
 
           {lastUpdated && (
             <div className="last-updated-text">
